198c5619bf9e5fe99d4ddb8940ae775bc016e285
Armando Jr <elfiservice@hotmail.com>
2024-04-21 11:45:18 -0300

    implemented the Update method

--- src/client/PartsClient/PartsClient/Data/PartsManager.cs
+++ src/client/PartsClient/PartsClient/Data/PartsManager.cs
@@ -82,7 +82,15 @@ public static class PartsManager
 
     public static async Task Update(Part part)
     {
-        throw new NotImplementedException();
+        if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            return;
+        
+        HttpRequestMessage msg = new(HttpMethod.Put, $"{Url}parts/{part.PartID}");
+        msg.Content = JsonContent.Create<Part>(part);
+        
+        var client = await GetClient();
+        var response = await client.SendAsync(msg);
+        response.EnsureSuccessStatusCode();
     }
 
     public static async Task Delete(string partID)
